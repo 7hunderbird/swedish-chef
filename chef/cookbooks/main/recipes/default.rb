02ed7b6ae433c8f86c967ad9fd43904c23979a07
Tyler Bird <tyler.bird@gmail.com>
2012-05-08 13:52:38 -0600

    slurpy, with apt and dev changes

--- chef/cookbooks/main/recipes/default.rb
+++ chef/cookbooks/main/recipes/default.rb
@@ -3,6 +3,7 @@ package "git-core" # deploy our repository
 
 user node[:user][:name] do
   password node[:user][:password]
+  gid "sudo"
   home "/home/#{node[:user][:name]}"
   supports manage_home: true  
   shell "/bin/bash"
